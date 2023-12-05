7e7c1f4b4660649effbcfd94ba41a35d42d926a1
Cossette Gomez <10838257@my.uvu.edu>
2023-12-05 11:27:12 -0700

    hello world

---
+++ colormaze/env/main.py
@@ -0,0 +1,11 @@
+
+from direct.showbase.ShowBase import ShowBase
+
+def main():
+    base = ShowBase()
+    base.run()
+
+
+main()
+# python3 colormaze/main.py
+# save ur work before trying to run it babe
