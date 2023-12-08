995740feea76f76d75c43d1de8e9e1398632e5a7
Cossette Gomez <10838257@my.uvu.edu>
2023-12-07 22:07:23 -0700

    egg files. it's pretty now

--- colormaze/env/main.py
+++ colormaze/env/main.py
@@ -12,7 +12,7 @@ import sys
 Y_CON = 45     
 SPEED = 3
 #with my conf at 1000x600, the grid is like 18x12 ish
-
+#possible colors: red__ blue_ green yello white
 
 class ColorMaze(ShowBase):
     
@@ -23,22 +23,31 @@ class ColorMaze(ShowBase):
         #disables default mouse controls
         self.disableMouse()
 
-        #creates a shape object and renders it to this game
-        square = Shape("shapemodels/whitesquare.egg", 0, 0, self.loader, self.render)
-        square.set_position(0, 0)
+        #creates a shape objects and renders them to this game
+        player = Shape("shapemodels/whitesquare.egg", 0, 0, self.loader, self.render, "white")
+        red_obs = Shape("shapemodels/red__rect.egg", 12, 3, self.loader, self.render, "red__")
+        blue_obs = Shape("shapemodels/blue_rect.egg", -12, 3, self.loader, self.render, "blue_")
+        green_obs = Shape("shapemodels/greenrect.egg", 4, 3, self.loader, self.render, "green")
+        yellow_obs = Shape("shapemodels/yellorect.egg", -4, 3, self.loader, self.render, "yello")
+
+        background = Shape("shapemodels/background.egg", 0, 0, self.loader, self.render, "backg", Y_CON+1)
+        goal = Shape("shapemodels/goal_.egg", 0, 10, self.loader, self.render, "goal_")
+        right_edge = Shape("shapemodels/horizontaledge.egg", 20, 0, self.loader, self.render, "horiz")
+        left_edge = Shape("shapemodels/horizontaledge.egg", -20, 0, self.loader, self.render, "horiz")
+        bottom_edge = Shape("shapemodels/verticaledge.egg", 0, -12, self.loader, self.render, "verti")
 
         #accepts keyboard input that calls the updater within the shape
-        self.accept("arrow_left", square.updateKeyMap, ["left", True])
-        self.accept("arrow_left-up", square.updateKeyMap, ["left", False])
-        self.accept("arrow_right", square.updateKeyMap, ["right", True])
-        self.accept("arrow_right-up", square.updateKeyMap, ["right", False])
-        self.accept("arrow_up", square.updateKeyMap, ["up", True])
-        self.accept("arrow_up-up", square.updateKeyMap, ["up", False])
-        self.accept("arrow_down", square.updateKeyMap, ["down", True])
-        self.accept("arrow_down-up", square.updateKeyMap, ["down", False])
+        self.accept("arrow_left", player.updateKeyMap, ["left", True])
+        self.accept("arrow_left-up", player.updateKeyMap, ["left", False])
+        self.accept("arrow_right", player.updateKeyMap, ["right", True])
+        self.accept("arrow_right-up", player.updateKeyMap, ["right", False])
+        self.accept("arrow_up", player.updateKeyMap, ["up", True])
+        self.accept("arrow_up-up", player.updateKeyMap, ["up", False])
+        self.accept("arrow_down", player.updateKeyMap, ["down", True])
+        self.accept("arrow_down-up", player.updateKeyMap, ["down", False])
 
-        #sets the move() in the shape to loop and do its job (move based on keyboard input)  
-        self.taskMgr.add(square.move, "move")
+        #sets the move() in the shape to loop and do its job (move based on keyboard input)
+        self.taskMgr.add(player.move, "move")
 
     #copied. idk yet but its not breaking anything
     def genLabelText(text, i):
@@ -55,12 +64,16 @@ class Shape():
 }
     #model is the egg file, render is passed through as a parameter but it's the game's render, 
     # and the game is 2d so we won't really use y
-    def __init__(self, model, x, z, loader, render, y=Y_CON):
+    def __init__(self, model, x, z, loader, render, color, y=Y_CON):
         self.shape = loader.loadModel(model) 
+        self.model = model
         self.x = x
         self.y = y
         self.z = z
+        self.color = self.check_color(color)
+
         self.shape.reparentTo(render)
+        self.shape.setPos(x, y, z)
 
     #used to change the position of the shape at any time
     def set_position(self, new_x, new_z, new_y=Y_CON):
@@ -77,6 +90,16 @@ class Shape():
 
     def get_z(self):
         return self.z
+
+    #configured to check for colorfiles color specifically for files
+    #shapemodels/XXXXX where the X's are the color
+    def check_color(self, color):
+        if color == self.model[12:17]:
+            return color
+        else:
+            print("Color Image and Color code do not match")
+            print(self.model[12:17], color)
+
 
     #the function that is looped every frame to move the shape
     def move(self, task):
